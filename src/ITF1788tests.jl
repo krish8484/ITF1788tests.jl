3bb9894187e8da0cfcd8ac1c93817fdd72cd27aa
Krish Agarwal <akrish136@gmail.com>
2020-02-29 22:27:41 +0530

    update add.jl

--- src/ITF1788tests.jl
+++ src/ITF1788tests.jl
@@ -1,9 +1,7 @@
 module ITF1788tests
 
 include("add.jl")
-
-
-    
+   
 export regenerate_test
 
 end # module
--- src/add.jl
+++ src/add.jl
@@ -1,6 +1,20 @@
 
-
-
 function regenerate_test()
     download("https://drive.google.com/u/0/uc?id=1jx7FZi9HwGq3gJPc4fjjI1bLxjmZ5Rrn&export=download", "fi_lib.jl")
+    download("https://drive.google.com/u/0/uc?id=1wpeFbQ3lW59WVi2NTl9XjXGb8xNyACwQ&export=download", "abs_rev.jl")
+    download("https://drive.google.com/u/0/uc?id=13bgyu2o0lE6vbChBp9l7VRD-GktzWbFP&export=download", "atan2.jl")
+    download("https://drive.google.com/u/0/uc?id=1lNVYLO2MoxpK_t8HZevw4Kyb-Xq49aSN&export=download", "c-xsc.jl")
+    download("https://drive.google.com/u/0/uc?id=13oYgM8lkp4KK5VHc15Mp-PqNwU0BA3MW&export=download", "ieee1788-constructors.jl")
+    download("https://drive.google.com/u/0/uc?id=1Yzq4PZxSz0rdMDcZelR3DxcpNlxPcyKA&export=download", "ieee1788-exceptions.jl")
+    download("https://drive.google.com/u/0/uc?id=1CtoBPf_yUErJr78sNCBy78CD9v00OOFz&export=download", "libieeep1788_bool.jl")
+    download("https://drive.google.com/u/0/uc?id=1FUltmfYTbkF6kDdV5Smh6naulID5J2_f&export=download", "libieeep1788_cancel.jl")
+    download("https://drive.google.com/u/0/uc?id=1F3ySXep023Bff3apm5M9DjJaOlVYi6c2&export=download", "libieeep1788_class.jl")
+    download("https://drive.google.com/u/0/uc?id=1IOqjsxUPg0LVh7-K2pC3lBxB9_dh_cp-&export=download", "libieeep1788_elem.jl")
+    download("https://drive.google.com/u/0/uc?id=1NSKDxu7mMevG0_YWn91YsQthyu_rORY3&export=download", "libieeep1788_num.jl")
+    download("https://drive.google.com/u/0/uc?id=1BizPXSTzg6Pli9PzOWjXHqpg8UiGubPz&export=download", "libieeep1788_overlap.jl")
+    download("https://drive.google.com/u/0/uc?id=1r6gW6ssTwYbvZOK1pSk2thidSJSEnA97&export=download", "libieeep1788_rec_bool.jl")
+    download("https://drive.google.com/u/0/uc?id=1EBdvzA5YTXFJAqHdEX0zn5BGf8fgnO8o&export=download", "libieeep1788_reduction.jl")
+    download("https://drive.google.com/u/0/uc?id=1hYiYVWFZGaFnxpdOv2358h6SrzYQkJSy&export=download", "libieeep1788_rev.jl")
+    download("https://drive.google.com/u/0/uc?id=1XxvBmdCiCwUrtNn9qN1gH2l5hjroGYjD&export=download", "libieeep1788_set.jl")
+    download("https://drive.google.com/u/0/uc?id=1Ci6nPtdl2MkTi0ethqJXHIKdGEJveO8x&export=download", "mpfi.jl")
 end
